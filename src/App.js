a1e8c5d4b504694e65be23c81cb4c10905d7bf9f
Amol Chitale <amolchitaledcjr@gmail.com>
2024-05-04 19:05:16 +0530

    search filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { Autocomplete, TextField } from "@mui/material";
+import "./styles/App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const roles = ["frontend", "backend", "dba"];
+  const noEmp = ["0-10", "10-20", "20-50"];
+  const exp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const rem = ["Remote", "Hybrid", "In-Office"];
+  const basePay = ["0L", "10L", "20L"];
+
+  const [data,setData] =  useState(null);
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const body = JSON.stringify({
+      limit: 10,
+      offset: 0,
+    });
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+
+    fetch(
+      "https://api.weekday.technology/adhoc/getSampleJdJSON",
+      requestOptions
+    )
+      .then((res) => res.text())
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(()=>{
+    
+  },[data])
+
   return (
-    <>
-      Hey
+    (data===null)?
+      <></>
+    :<>
+      <div className="searchBox">
+        <Autocomplete
+          multiple
+          limitTags={2}
+          id="multiple-limit-tags"
+          options={roles}
+          getOptionLabel={(option) => option}
+          renderInput={(params) => (
+            <TextField {...params} label="Role" placeholder="" />
+          )}
+          sx={{ minWidth: 150 }}
+        />
+        <Autocomplete
+          multiple
+          limitTags={2}
+          id="multiple-limit-tags"
+          options={noEmp}
+          getOptionLabel={(option) => option}
+          renderInput={(params) => (
+            <TextField {...params} label="Number Of Employees" />
+          )}
+          sx={{ minWidth: 220 }}
+        />
+        <Autocomplete
+          id="multiple-limit-tags"
+          options={exp}
+          getOptionLabel={(option) => option}
+          renderInput={(params) => <TextField {...params} label="Experience" />}
+          sx={{ minWidth: 150 }}
+        />
+        <Autocomplete
+          multiple
+          id="multiple-limit-tags"
+          options={rem}
+          getOptionLabel={(option) => option}
+          renderInput={(params) => <TextField {...params} label="Remote" />}
+          sx={{ minWidth: 150 }}
+        />
+        <Autocomplete
+          id="multiple-limit-tags"
+          options={basePay}
+          getOptionLabel={(option) => option}
+          renderInput={(params) => (
+            <TextField {...params} label="Minimum Base Pay Salary" />
+          )}
+          sx={{ minWidth: 300 }}
+        />
+        <TextField label="Search Company Name" />
+      </div>
     </>
   );
 }
